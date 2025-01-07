1f2f29198649356a646d28752eda83bbdaaeeea4
Sonamrit <lama77916@gmail.com>
2025-01-07 17:38:50 +0545

    Initial commit

---
+++ Restaurant_Projects/Base_App/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def HomeView(request):
+    return render(request,'home.html')
+
+def AboutView(request):
+    pass
+
+def MenuView(request):
+    pass
+
+def BookTableView(request):
+    pass
